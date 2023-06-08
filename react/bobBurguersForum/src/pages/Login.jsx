94e45366d7171dfa90c0f587c32cab453dd751f6
simonpatrickreyes <simonpatrickreyes@gmail.com>
2023-06-08 11:04:38 +0200

    login

--- react/bobBurguersForum/src/pages/Login.jsx
+++ react/bobBurguersForum/src/pages/Login.jsx
@@ -1,41 +1,72 @@
-import React from 'react'
-import { Formik } from 'formik';
-
+import React from 'react';
+import { Formik, Form, Field } from 'formik';
 
 const Login = () => {
   return (
     <>
       <Formik
         initialValues={{
-          nombre:'',
-          email:'',
-          password:''
+          nombre: '',
+          email: '',
+          password: ''
         }}
-        onSubmit={() => {
+
+        validate={(valores)=>{
+          let errors ={};
+
+          if (!valores.nombre) {
+            errors.nombre='Escribe un nombre'
+          } else if(!/^[a-zA-ZÀ-ÿ\s]{1,40}$/.test(valores.nombre)){
+            errors.nombre = "El nombre solo puede contener letras y espacios"
+          }
+
+          return errors 
+        }}
+
+        onSubmit={(valores) => {
           console.log('Formulario enviado');
         }}
       >
-        {({values, handleSubmit}) => {
-          <form className='formLogin' onSubmit={handleSubmit}>
+        {({ values, errors, handleSubmit, handleChange, handleBlur }) => (
+          <Form className="formLogin" onSubmit={handleSubmit}>
             <div>
               <label htmlFor="nombre">Nombre</label>
-              <input type="text" id='nombre' name='nombre' value={values.nombre} />
+              <Field 
+              type="text" 
+              id="nombre" 
+              name="nombre" 
+              onChange={handleChange} 
+              onBlur={handleBlur}
+              className="formLogin__field"/>
+              {errors.nombre && <div className='error'>{errors.nombre}</div>}
             </div>
             <div>
               <label htmlFor="email">email</label>
-              <input type="email" id='email' name='email'  value={values.email}/>
+              <Field 
+              type="email"
+               id="email" 
+               name="email" 
+               onChange={handleChange}
+               onBlur={handleBlur} 
+               className="formLogin__field"/>
             </div>
             <div>
               <label htmlFor="password">password</label>
-              <input type="password" id='password' name='password' value={values.password}/>
+              <Field 
+              type="password" 
+              id="password" 
+              name="password" 
+              onBlur={handleBlur}
+              onChange={handleChange} 
+              className="formLogin__field"/>
             </div>
-            <button type='submit'>Enviar</button>
-          </form>
-        }}
+            <button type="submit">Enviar</button>
+          </Form>
+        )}
       </Formik>
     </>
   );
-}
-
+};
 
 export default Login;
+
